93c4d2eb53367a2c2ecbe5d60f9e27eaac8500dd
nori3tsu <tugend.licht@gmail.com>
2013-11-18 16:41:10 +0900

    change install script

--- install.sh
+++ install.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
+# .dotfiles
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Installing dotfiles for the first time"
     git clone https://github.com/norilicht/dotfiles "$HOME/.dotfiles"
-    cd "$HOME/.dotfiles"
-    
-    # NeoBundle
-    mkdir -p $HOME/.vim/bundle
-    git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
-    
-    # vimrc
-    ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+
 else
     echo "dotfiles is already installed"
+fi
+
+# NeoBundle
+if [ ! -d "$HOME/.vim/bundle" ]; then
+    mkdir -p $HOME/.vim/bundle
+    git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+else
+    echo "NeoBundle is already installed"
+fi
+
+# vimrc
+if [ ! -f "$HOME/.vimrc" ]; then
+    ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+else
+    echo ".vimrc is already installed"
+fi
+
+# tmux
+if [ ! -f "$HOME/.tmux.conf" ]; then
+    ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+else
+    echo ".tmux.conf is already installed"
 fi
