2b0f1f2647518d6bd70b19c907393a43e5f32c61
nori3tsu <tugend.licht@gmail.com>
2015-05-12 10:44:46 +0900

    Refactor brew-installer.sh

--- brew-installer.sh
+++ brew-installer.sh
@@ -2,81 +2,97 @@
 
 brew tap homebrew/binary
 brew tap caskroom/versions
-
-brew install awscli
-brew install colordiff
-brew install coreutils
-brew install ctags
-brew install erlang
-brew install git
-brew install gnu-tar
-brew install go
-brew install gpg
-brew install gradle
-brew install heroku
-brew install imagemagick
-brew install jq
-brew install ngrep
-brew install node
-brew install openssl
-brew install peco
-brew install rbenv
-brew install reattach-to-user-namespace
-brew install ruby-build
-brew install shellcheck
-brew install siege
-brew install terminal-notifier
-brew install tmux
-brew install tsung
-brew install vim
-brew install zsh
-brew install zsh-completions
-
 brew install Caskroom/cask/tuntap
 brew install caskroom/cask/brew-cask
 
+install() {
+  local pkg=$1
+  if [[ ! -d /usr/local/Cellar/$pkg ]]; then
+    brew install "$pkg"
+  else
+    echo "$pkg is already installed."
+  fi
+}
 
-brew cask install alfred
-brew cask install appcleaner
-brew cask install asteroid
-brew cask install bartender
-brew cask install bettertouchtool
-brew cask install betterzipql
-brew cask install cornerstone
-brew cask install cyberduck
-brew cask install dash
-brew cask install dropbox
-brew cask install easysimbl
-brew cask install filezilla
-brew cask install firefox
-brew cask install gimp
-brew cask install github
-brew cask install goofy
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install google-japanese-ime
-brew cask install gyazo
-brew cask install handbrake
-brew cask install hyperswitch
-brew cask install ifunbox
-brew cask install insomniax
-brew cask install iterm2
-brew cask install karabiner
-brew cask install kindle
-brew cask install libreoffice
-brew cask install macvim
-brew cask install electrum
-brew cask install pandoc
-brew cask install qlcolorcode
-brew cask install qlmarkdown
-brew cask install qlstephen
-brew cask install quicklook-csv
-brew cask install quicklook-json
-brew cask install shiftit
-brew cask install skype
-brew cask install slack
-brew cask install sourcetree
-brew cask install thunderbird
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install xquartz
+install awscli
+install colordiff
+install coreutils
+install ctags
+install erlang
+install git
+install gnu-tar
+install go
+install gnupg
+install gradle
+install heroku-toolbelt
+install imagemagick
+install jq
+install ngrep
+install node
+install openssl
+install peco
+install rbenv
+install reattach-to-user-namespace
+install ruby-build
+install shellcheck
+install siege
+install terminal-notifier
+install tmux
+install tsung
+install vim
+install zsh
+install zsh-completions
+
+cask_install() {
+  local pkg=$1
+  if [[ ! -d /opt/homebrew-cask/Caskroom/$pkg ]]; then
+    brew cask install "$pkg"
+  else
+    echo "$pkg is already installed."
+  fi
+}
+
+cask_install alfred
+cask_install appcleaner
+cask_install asteroid
+cask_install bartender
+cask_install bettertouchtool
+cask_install betterzipql
+cask_install cornerstone
+cask_install cyberduck
+cask_install dash
+cask_install dropbox
+cask_install easysimbl
+cask_install filezilla
+cask_install firefox
+cask_install gimp
+cask_install github
+cask_install goofy
+cask_install google-chrome
+cask_install google-drive
+cask_install google-japanese-ime
+cask_install gyazo
+cask_install handbrake
+cask_install hyperswitch
+cask_install ifunbox
+cask_install insomniax
+cask_install iterm2
+cask_install karabiner
+cask_install kindle
+cask_install libreoffice
+cask_install macvim
+#cask_install electrum
+cask_install pandoc
+cask_install qlcolorcode
+cask_install qlmarkdown
+cask_install qlstephen
+cask_install quicklook-csv
+cask_install quicklook-json
+cask_install shiftit
+cask_install skype
+cask_install slack
+cask_install sourcetree
+cask_install thunderbird
+cask_install vagrant
+cask_install virtualbox
+cask_install xquartz
